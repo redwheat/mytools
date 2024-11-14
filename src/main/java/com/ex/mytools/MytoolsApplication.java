eb0526f110b2a37866deb72f162b61b6f1e4f55d
zhengbingyuan@leyopharm.com <1551907870@qq.com>
2024-11-14 08:59:33 +0800

    新功能: 初始化项目

---
+++ src/main/java/com/ex/mytools/MytoolsApplication.java
@@ -0,0 +1,13 @@
+package com.ex.mytools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytoolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MytoolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ex/mytools/MytoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.mytools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytoolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
